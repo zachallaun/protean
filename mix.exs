0eebdf2ef285f8b73357dfcac88e8e5128d06687
Zach Allaun <zach.allaun@gmail.com>
2022-07-16 20:56:05 -0400

    release: we're at version 0.0.1 now

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Protean.MixProject do
   def project do
     [
       app: :protean,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
