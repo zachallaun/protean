fefe074baa3ffd4879e6eb9e8948f02e59d9f60a
Zach Allaun <zach.allaun@gmail.com>
2022-07-19 18:04:23 -0400

    wip: testing delayed transitions

---
+++ test/protean/features/delayed_transition_test.exs
@@ -0,0 +1,65 @@
+defmodule Protean.Features.DelayedTransitionTest do
+  use ProteanTest.Case, async: true
+
+  defmodule TestMachine do
+    use Protean
+
+    @machine [
+      context: %{
+        path: []
+      },
+      initial: :a,
+      states: [
+        a: [
+          entry: ["save_path"],
+          after: [
+            delay: 20,
+            target: :b
+          ]
+        ],
+        b: [
+          entry: ["save_path"],
+          after: [
+            delay: 20,
+            target: :c
+          ]
+        ],
+        c: [
+          entry: ["save_path"]
+        ],
+        d: [
+          entry: ["save_path"]
+        ]
+      ],
+      on: [
+        goto_c: ".c",
+        goto_d: ".d"
+      ]
+    ]
+
+    def pure("save_path", state, %{path: path}, _) do
+      Protean.Action.assign(state, :path, [state.value | path])
+    end
+  end
+
+  @tag machine: TestMachine
+  test "takes automatic delayed transitions", %{machine: machine} do
+    assert Protean.matches?(machine, :a)
+    :timer.sleep(20)
+    assert Protean.matches?(machine, :b)
+    :timer.sleep(20)
+    assert Protean.matches?(machine, :c)
+  end
+
+  @tag machine: TestMachine
+  test "delayed transitions can be short-circuited by transitioning early", %{machine: machine} do
+    Protean.send(machine, "goto_c")
+    :timer.sleep(20)
+    assert Protean.matches?(machine, :c)
+  end
+
+  @tag machine: TestMachine
+  test "short-circuited transitions don't execute actions", %{machine: machine} do
+    assert [[["a", "#"]]] = Protean.current(machine).context
+  end
+end
