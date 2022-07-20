c72aad46d80f5e7dca9c81a3fbb4d9aea680da00
Zach Allaun <zach.allaun@gmail.com>
2022-07-19 21:02:25 -0400

    test: automatic transitions

---
+++ test/integration/automatic_transition_test.exs
@@ -0,0 +1,92 @@
+defmodule ProteanIntegration.AutomaticTransitionTest do
+  use Protean.TestCase
+
+  defmodule TestMachine do
+    use Protean
+    alias Protean.Action
+
+    @machine [
+      initial: :a,
+      context: %{
+        acc: [],
+        allow: false
+      },
+      states: [
+        a: [
+          on: [
+            goto_b: "b"
+          ]
+        ],
+        b: [
+          always: [
+            target: "c",
+            actions: ["auto_to_c"]
+          ]
+        ],
+        c: [
+          always: [
+            target: "d",
+            actions: ["auto_to_d"]
+          ]
+        ],
+        d: [
+          always: [
+            target: "a",
+            when: "allow?"
+          ],
+          on: [
+            allow: [
+              target: "e",
+              actions: [Action.assign(allow: true)]
+            ]
+          ]
+        ],
+        e: []
+      ],
+      on: [
+        goto_a: ".a",
+        outer_allow: [
+          actions: [Action.assign(allow: true)]
+        ]
+      ]
+    ]
+
+    @impl true
+    def pure(action_name, state, %{acc: acc}) do
+      Action.assign(state, :acc, [action_name | acc])
+    end
+
+    @impl true
+    def condition("allow?", _state, %{allow: true}, _event), do: true
+  end
+
+  @moduletag machine: TestMachine
+
+  test "automatic transitions trigger actions in correct order", %{machine: machine} do
+    assert_protean(machine,
+      send: "goto_b",
+      matches?: "d",
+      context: [acc: ["auto_to_d", "auto_to_c"]]
+    )
+  end
+
+  test "normal transitions take precedence even if auto condition is true", %{machine: machine} do
+    assert_protean(machine,
+      send: "goto_b",
+      send: "allow",
+      matches?: "e",
+      send: "goto_a",
+      send: "goto_b",
+      matches?: "a"
+    )
+  end
+
+  test "auto transition triggered when condition is met", %{machine: machine} do
+    assert_protean(machine,
+      send: "goto_b",
+      matches?: "d",
+      send: "outer_allow",
+      matches?: "a"
+    )
+  end
+end
