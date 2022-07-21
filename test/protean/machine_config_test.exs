ec6c3724bb99b1fafbaf653259dce31227c0449a
Zach Allaun <zach.allaun@gmail.com>
2022-07-21 15:32:58 -0400

    test: refactor machine config test

--- test/protean/machine_config_test.exs
+++ test/protean/machine_config_test.exs
@@ -1,13 +1,87 @@
 defmodule Protean.MachineConfigTest do
   use ExUnit.Case
 
-  setup context do
-    TestMachines.with_test_machine(context)
+  alias Protean.Action
+  alias Protean.MachineConfig
+
+  def delayed_transition_implicit do
+    [
+      initial: :a,
+      states: [
+        a: [
+          after: [
+            delay: 2000,
+            target: :b
+          ],
+          on: [
+            goto_c: :c
+          ]
+        ],
+        b: [
+          after: [
+            [
+              delay: 1000,
+              when: "some_condition",
+              target: :c
+            ],
+            [
+              delay: 2000,
+              target: :c
+            ]
+          ]
+        ],
+        c: []
+      ]
+    ]
   end
 
-  @tag machines: [:delayed_transition_machine_implicit, :delayed_transition_machine_explicit]
-  test "delayed transitions are just syntax sugar", %{machines: machines} do
-    [%{machine: implicit}, %{machine: explicit}] = machines
-    assert implicit.root == explicit.root
+  def delayed_transition_explicit do
+    [
+      initial: :a,
+      states: [
+        a: [
+          entry: [
+            Action.send_event("$protean.after.2000-#a", delay: 2000)
+          ],
+          exit: [
+            Action.cancel_event("$protean.after.2000-#a")
+          ],
+          on: [
+            "$protean.after.2000-#a": [
+              target: :b
+            ],
+            goto_c: :c
+          ]
+        ],
+        b: [
+          entry: [
+            Action.send_event("$protean.after.1000-#b", delay: 1000),
+            Action.send_event("$protean.after.2000-#b", delay: 2000)
+          ],
+          exit: [
+            Action.cancel_event("$protean.after.1000-#b"),
+            Action.cancel_event("$protean.after.2000-#b")
+          ],
+          on: [
+            "$protean.after.1000-#b": [
+              target: :c,
+              when: "some_condition"
+            ],
+            "$protean.after.2000-#b": [
+              target: :c
+            ]
+          ]
+        ],
+        c: []
+      ]
+    ]
+  end
+
+  test "delayed transitions syntax" do
+    [explicit, implicit] =
+      [delayed_transition_explicit(), delayed_transition_implicit()]
+      |> Enum.map(&MachineConfig.parse!/1)
+
+    assert explicit == implicit
   end
 end
