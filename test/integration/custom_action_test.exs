af6c2d107743dad710ca4d6cbb8cb2d244a9f14b
Zach Allaun <zach.allaun@gmail.com>
2022-07-29 23:16:00 -0400

    test: integration test for custom halting action

---
+++ test/integration/custom_action_test.exs
@@ -0,0 +1,44 @@
+defmodule ProteanIntegration.CustomActionTest do
+  use Protean.TestCase
+
+  defmodule CustomAction do
+    @behaviour Protean.Action
+
+    def custom do
+      {__MODULE__, :custom}
+    end
+
+    def exec_action(:custom, interpreter) do
+      {:halt, interpreter}
+    end
+  end
+
+  defmodule TestMachine do
+    use Protean
+
+    @machine [
+      initial: "init",
+      context: [data: nil],
+      states: [
+        init: [
+          entry: [
+            CustomAction.custom(),
+            Protean.Action.assign(data: :ok)
+          ],
+          on: [
+            event: []
+          ]
+        ]
+      ]
+    ]
+  end
+
+  @tag machine: TestMachine
+  test "custom action halts action pipeline", %{machine: machine} do
+    assert_protean(machine,
+      context: [data: nil],
+      send: "event",
+      context: [data: nil]
+    )
+  end
+end
