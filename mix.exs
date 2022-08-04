0108ac727ce586fa3f7fcad64dffa229bac6728a
Zach Allaun <zach.allaun@gmail.com>
2022-08-04 15:05:01 -0400

    build: 0.0.2

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Protean.MixProject do
   use Mix.Project
 
   @name "Protean"
-  @version "0.0.1"
+  @version "0.0.2"
   @source_url "https://github.com/zachallaun/protean"
 
   def project do
