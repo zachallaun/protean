4f6f17490c5ae2492f63fdc7d4cfb271b869aab4
Zach Allaun <zach.allaun@gmail.com>
2022-08-13 12:49:19 -0400

    build: add dialyxir to test env for ci

--- mix.exs
+++ mix.exs
@@ -48,7 +48,7 @@ defmodule Protean.MixProject do
     [
       {:credo, "~> 1.6", only: :dev, runtime: false},
       {:ex_doc, "~> 0.28.4", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.2", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.2", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.10", only: :test}
     ]
   end
